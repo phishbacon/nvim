fdf1dc0e72da728ec030878c47b8f05299beb727
phishbacon <thechristianreynolds@gmail.com>
2025-02-03 21:25:58 -0500

    Use browser for peek

--- lua/phishbacon/plugins/markdown.lua
+++ lua/phishbacon/plugins/markdown.lua
@@ -4,6 +4,7 @@ return {
   build = "deno task --quiet build:fast",
   config = function()
     require("peek").setup({
+      app = "browser",
       filetype = { "markdown" }
     })
     vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
