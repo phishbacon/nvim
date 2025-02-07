93ae276e59a3f8e06f2e53da302cd590289305c4
phishbacon <thechristianreynolds@gmail.com>
2025-02-07 01:03:37 -0500

    Add random themes and indent lines

--- lua/phishbacon/plugins/colorscheme.lua
+++ lua/phishbacon/plugins/colorscheme.lua
@@ -1,9 +1,34 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  config = function()
-    require("tokyonight").setup({
-      vim.cmd [[colorscheme tokyonight-night]]
-    })
-  end
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    config = function()
+      require("tokyonight").setup({
+        ---@diagnostic disable-next-line: missing-fields
+        vim.cmd [[colorscheme tokyonight-night]]
+      })
+    end
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    -- config = function()
+    --   vim.cmd [[colorscheme moonfly]]
+    -- end
+  },
+  {
+    'ray-x/aurora',
+    init = function()
+      vim.g.aurora_italic = 1
+      vim.g.aurora_transparent = 1
+      vim.g.aurora_bold = 1
+    end,
+    -- config = function()
+    --   vim.cmd.colorscheme "aurora"
+    --   -- override defaults
+    --   vim.api.nvim_set_hl(0, '@number', { fg = '#e933e3' })
+    -- end
+  }
 }
--- lua/phishbacon/plugins/indent-blankline.lua
+++ lua/phishbacon/plugins/indent-blankline.lua
@@ -15,7 +15,7 @@ return {
       "RainbowCyan",
     }
 
-    local hooks = require "ibl.hooks"
+    local hooks = require("ibl.hooks")
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
@@ -28,7 +28,33 @@ return {
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
     require("ibl").setup({
-      scope = { highlight = highlight },
+      scope = {
+        highlight = highlight,
+        enabled = true
+      },
     })
+    local onConfig = require("ibl.config").config
+    if onConfig then
+      print(onConfig.indent.char)
+    end
+    local offConfig = {
+      indent = {
+        char = "",
+      },
+      scope = {
+        highlight = highlight,
+        enabled = false
+      }
+    }
+    local update = require("ibl").update
+    update(offConfig)
+    vim.keymap.set("n", "<leader>hl", function()
+      local config = require("ibl.config").config
+      if config and onConfig and config.scope.enabled then
+        update(offConfig)
+      elseif config and onConfig then
+        update(onConfig)
+      end
+    end)
   end
 }
