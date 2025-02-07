379eeb29eb9e95e923cbe00b0c0c8faa8e6dd143
phishbacon <thechristianreynolds@gmail.com>
2025-02-06 23:51:27 -0500

    Remove neo-tree cuz trees are bad

--- lua/phishbacon/plugins/mini.lua
+++ lua/phishbacon/plugins/mini.lua
@@ -5,19 +5,6 @@ return {
     require("mini.statusline").setup()
     require("mini.comment").setup()
     require("mini.pairs").setup()
-    require("mini.indentscope").setup({
-      draw = {
-        delay = 0
-      }
-    })
-    require("mini.tabline").setup({
-      vim.api.nvim_create_autocmd("Filetype", {
-        pattern = "neo-tree",
-        group = vim.api.nvim_create_augroup("FiletypeGroup", { clear = true }),
-        callback = function(args)
-          vim.b[args.buf].ministatusline_disable = true
-        end
-      })
-    })
+    require("mini.tabline").setup()
   end
 }
--- lua/phishbacon/plugins/telescope.lua
+++ lua/phishbacon/plugins/telescope.lua
@@ -1,28 +1,53 @@
 return {
+  { "nvim-telescope/telescope-ui-select.nvim" },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "dropdown"
+          },
+          live_grep = {
+            theme = "dropdown"
+          },
+          help_tags = {
+            theme = "dropdown"
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({})
+          },
+        },
+      })
+
+      require("telescope").load_extension("ui-select")
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>fn",
+        function()
+          builtin.find_files({
+            cwd = vim.fn.stdpath("config")
+          })
+        end,
+        { desc = "Telescope find in nvim config" })
+      vim.keymap.set("n", "<leader>fp",
+        function()
+          builtin.find_files({
+            ---@diagnostic disable-next-line: param-type-mismatch
+            cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+          })
+        end,
+        { desc = "Telescope find in nvim config" })
     end
   },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  }
 }
