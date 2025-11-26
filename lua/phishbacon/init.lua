d0329c9d4e3eab1a8061046e8436dfd30f287809
phishbacon <thechristianreynolds@gmail.com>
2025-11-26 11:04:51 -0500

    Add vscode check

--- lua/phishbacon/init.lua
+++ lua/phishbacon/init.lua
@@ -2,30 +2,31 @@ require("phishbacon.options")
 require("phishbacon.keymap")
 require("phishbacon.lazy")
 
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
-autocmd("LspAttach", {
-  group = augroup("LspGroup", { clear = true }),
-  callback = function(args)
-    local opts = { buffer = args.buf }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "List code actions" })
-    vim.keymap.set("n", "<leader>dd", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic float" })
-    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, { desc = "View references" })
-    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, { desc = "Rename reference" })
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic_goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic_goto_prev() end, opts)
-  end
-})
-
-autocmd("TextYankPost", {
-  desc = "Pretty colors when yanking",
-  group = augroup("HighlightGroup", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end
-})
-
+if not vim.g.vscode then
+  local autocmd = vim.api.nvim_create_autocmd
+  local augroup = vim.api.nvim_create_augroup
+  
+  autocmd("LspAttach", {
+    group = augroup("LspGroup", { clear = true }),
+    callback = function(args)
+      local opts = { buffer = args.buf }
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "List code actions" })
+      vim.keymap.set("n", "<leader>dd", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic float" })
+      vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, { desc = "View references" })
+      vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, { desc = "Rename reference" })
+      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic_goto_next() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic_goto_prev() end, opts)
+    end
+  })
+  
+  autocmd("TextYankPost", {
+    desc = "Pretty colors when yanking",
+    group = augroup("HighlightGroup", { clear = true }),
+    callback = function()
+      vim.highlight.on_yank()
+    end
+  })
+end
